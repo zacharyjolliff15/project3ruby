986064cb8b136dfd939376cb862da37115a96e24
zjolliff <zjolliff@zjolliffs-MacBook-Pro-2.local>
2025-12-10 22:09:41 -0500

    user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path, notice: "Account created! Please log in."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
