8460e49584c994f8ac2100c2fad1dd315632cca3
zjolliff <zjolliff@zjolliffs-MacBook-Pro-2.local>
2025-12-10 22:08:08 -0500

    post and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+
+  has_secure_password
+
+  before_save { self.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,  presence: true
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
 end
